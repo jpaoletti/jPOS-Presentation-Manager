2f8f9b8a4137e2afc0b2132a6098bfe5d2e6a7c0
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2010-02-21 22:45:52 -0300

    Removed javascript login encriptation. SSL do this better.

--- modules/pm_struts/webapps/pm/js/encrypt.js
+++ modules/pm_struts/webapps/pm/js/encrypt.js
@@ -1,14 +1,3 @@
 function encrypt(i_user, i_pass) {
-    var username = i_user.value;
-    var password = i_pass.value;
-
-    if (username.length < 3 || password.length < 3) {
-        alert ("Invalid Username and/or Password.");
-        return false;
-    }
-    var seed     = readCookie ("JSESSIONID");
-    var pass     = hex_md5 (username + password);
-
     i_pass.readOnly = true;
-    i_pass.value = hex_md5 (seed + pass);
 }
