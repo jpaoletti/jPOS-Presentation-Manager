2327e406b3682ff3f70a02a536be0bf95d15b768
Jerónimo Paoletti <jpaoletti@notebook-yero.(none)>
2010-03-24 15:55:59 -0300

    new module! Pentaho data integration module is made to integrate Pentaho report pages in PresentationManager. Its still under development and probably it just work for some special cases, but im working on it.

---
+++ modules/pentaho/src/org/jpos/ee/pm/pentaho/MenuItemLocationPentaho.java
@@ -0,0 +1,12 @@
+package org.jpos.ee.pm.pentaho;
+
+import org.jpos.ee.pm.menu.MenuItem;
+import org.jpos.ee.pm.struts.MenuItemLocationStruts;
+
+public class MenuItemLocationPentaho extends MenuItemLocationStruts {
+
+	protected String buildLink(MenuItem item, Object... params) {
+		String link = PentahoQBean.getService().getUrl()+item.getLocation_value();
+		return link;
+	}
+}
