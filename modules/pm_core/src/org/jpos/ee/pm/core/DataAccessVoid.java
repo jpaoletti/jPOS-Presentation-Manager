64017fbc59c5abd94dd864497f69863ec3fe87d5
Jeronimo Paoletti <jeronimo.paoletti@gmail.com>
2010-04-11 14:42:27 -0300

    Added a Void Data Access

---
+++ modules/pm_core/src/org/jpos/ee/pm/core/DataAccessVoid.java
@@ -0,0 +1,45 @@
+package org.jpos.ee.pm.core;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataAccessVoid implements DataAccess {
+
+	
+	public void add(PMContext ctx, Object instance) throws PMException {
+
+	}
+
+	
+	public Long count(PMContext ctx) throws PMException {
+		return 0L;
+	}
+
+	
+	public EntityFilter createFilter(PMContext ctx) throws PMException {
+		return new EntityFilter();
+	}
+
+	
+	public void delete(PMContext ctx, Object object) throws PMException {
+
+	}
+
+	
+	public Object getItem(PMContext ctx, String property, String value)throws PMException {
+		return null;
+	}
+
+	public List<?> list(PMContext ctx, EntityFilter filter, Integer from,Integer count) throws PMException {
+		return new ArrayList<Object>();
+	}
+
+	public Object refresh(PMContext ctx, Object o) throws PMException {
+		return o;
+	}
+
+	
+	public void update(PMContext ctx, Object instance) throws PMException {
+
+	}
+}
