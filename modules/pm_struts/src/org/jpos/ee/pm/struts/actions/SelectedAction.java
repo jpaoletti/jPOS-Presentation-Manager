e087ed541230e7452f5de707ceb0269e81b155a0
Jerónimo Paoletti <jpaoletti@notebook-yero.(none)>
2010-05-14 14:26:32 -0300

    Added SelectedAction

---
+++ modules/pm_struts/src/org/jpos/ee/pm/struts/actions/SelectedAction.java
@@ -0,0 +1,18 @@
+package org.jpos.ee.pm.struts.actions;
+
+import org.jpos.ee.pm.core.PMException;
+import org.jpos.ee.pm.struts.PMStrutsContext;
+
+/**This class is specific just for set selected item and use it in general propose 
+ * actions. Main functionality must be in jsp
+ * 
+ * @author jpaoletti
+ * */
+public class SelectedAction extends RowActionSupport {
+
+	@Override
+	protected void doExecute(PMStrutsContext ctx) throws PMException {
+		return;
+	}
+
+}
