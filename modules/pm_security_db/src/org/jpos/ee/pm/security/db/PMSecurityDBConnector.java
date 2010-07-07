0a6e59772815ec208dea842f32dbdd07caee9564
jpaoletti <jeronimo.paoletti@gmail.com>
2010-07-07 10:51:56 -0300

    adjustments to db security connector to allow a better subclassing.

--- modules/pm_security_db/src/org/jpos/ee/pm/security/db/PMSecurityDBConnector.java
+++ modules/pm_security_db/src/org/jpos/ee/pm/security/db/PMSecurityDBConnector.java
@@ -26,90 +26,97 @@ import org.jpos.ee.pm.security.core.UserNotFoundException;
 import org.jpos.util.Log;
 
 public class PMSecurityDBConnector implements PMSecurityConnector {
-    private PMSecurityService service;
-    private PMContext ctx;
-    
-    public PMSecurityDBConnector(){
-        
+
+    protected PMSecurityService service;
+    protected PMContext ctx;
+
+    public PMSecurityDBConnector() {
     }
-    
-    public void setService(PMSecurityService service){
+
+    public void setService(PMSecurityService service) {
         this.service = service;
     }
-    
-    protected Log getLog(){
+
+    protected Log getLog() {
         return service.getLog();
     }
-    
+
     /**Here i get plain password and i must encrypt it before compare*/
-    public PMSecurityUser authenticate(String username, String password)throws PMSecurityException{
+    public PMSecurityUser authenticate(String username, String password) throws PMSecurityException {
         PMSecurityUser user = getUser(username);
-        if(user == null) 
+        if (user == null) {
             throw new UserNotFoundException();
-        if(user.isDeleted())
+        }
+        if (user.isDeleted()) {
             throw new UserNotFoundException();
-        if(!user.isActive())
+        }
+        if (!user.isActive()) {
             throw new UserNotActiveException();
-        if (user.getPassword().compareTo(this.encryptPassword(username, password)) != 0)
+        }
+        if (user.getPassword().compareTo(this.encryptPassword(username, password)) != 0) {
             throw new InvalidPasswordException();
+        }
         return user;
     }
-    
-    public void changePassword(String username, String password, String newpassword) throws PMSecurityException{
+
+    public void changePassword(String username, String password, String newpassword) throws PMSecurityException {
         DB db = (DB) ctx.get(PM_DB);
-        try{
+        try {
             SECUser user = getDBUser(username);
-            if(password!=null){
-            	authenticate(username, password);
-            	checkUserRules(username, password);
+            if (password != null) {
+                authenticate(username, password);
+                checkUserRules(username, password);
             }
-            user.setPassword( encryptPassword(username, newpassword) );
+            user.setPassword(encryptPassword(username, newpassword));
             db.save(user);
-        } catch (PMSecurityException e ){
+        } catch (PMSecurityException e) {
             throw e;
         } catch (Exception e) {
             getLog().error(e);
             throw new PMSecurityException("unespected.error");
         }
     }
-    
-    private void checkUserRules(String username, String password) throws PMSecurityException {
-        if(username == null) throw new InvalidUserException();
-        if(password == null) throw new InvalidPasswordException();
+
+    protected void checkUserRules(String username, String password) throws PMSecurityException {
+        if (username == null) {
+            throw new InvalidUserException();
+        }
+        if (password == null) {
+            throw new InvalidPasswordException();
+        }
         //TODO Check rules
     }
 
-    private String encryptPassword(String username, String password) {
-        return ((new MD5_hex()).calcMD5(username+password));
+    protected String encryptPassword(String username, String password) {
+        return ((new MD5_hex()).calcMD5(username + password));
     }
 
-    public PMSecurityUser getUser(String username)                         throws PMSecurityException{
+    public PMSecurityUser getUser(String username) throws PMSecurityException {
         return convert(getDBUser(username));
     }
 
-    public SECUser getDBUser(String username){
+    public SECUser getDBUser(String username) {
         DB db = (DB) ctx.get(PM_DB);
         SECUser u = null;
         try {
-             u = (SECUser) db.session()
-                                        .createCriteria (SECUser.class)
-                                        .add (Restrictions.eq ("nick", username))
-                                        .uniqueResult();
-                
-            if(u!=null)db.session().refresh(u);
-                
+            u = (SECUser) db.session().createCriteria(SECUser.class).add(Restrictions.eq("nick", username)).uniqueResult();
+
+            if (u != null) {
+                db.session().refresh(u);
+            }
+
         } catch (Exception e) {
             getLog().error(e);
         }
         return u;
     }
-    
-    public List<PMSecurityUser> getUsers()                             throws PMSecurityException{
+
+    public List<PMSecurityUser> getUsers() throws PMSecurityException {
         List<PMSecurityUser> result = new ArrayList<PMSecurityUser>();
         DB db = (DB) ctx.get(PM_DB);
-        try{
+        try {
             List<SECUser> users = db.session().createCriteria(SECUser.class).list();
-            for(SECUser u : users){
+            for (SECUser u : users) {
                 result.add(convert(u));
             }
         } catch (Exception e) {
@@ -117,15 +124,16 @@ public class PMSecurityDBConnector implements PMSecurityConnector {
         }
         return result;
     }
-    
-    public void addUser(PMSecurityUser user)                            throws PMSecurityException{
+
+    public void addUser(PMSecurityUser user) throws PMSecurityException {
         DB db = (DB) ctx.get(PM_DB);
-        try{
-            if(getDBUser(user.getUsername().toLowerCase())!=null) 
+        try {
+            if (getDBUser(user.getUsername().toLowerCase()) != null) {
                 throw new UserAlreadyExistException();
+            }
             checkUserRules(user.getUsername(), user.getPassword());
-            SECUser secuser = unconvert(null,user);
-            secuser.setPassword( encryptPassword(user.getUsername(), user.getPassword()) );
+            SECUser secuser = unconvert(null, user);
+            secuser.setPassword(encryptPassword(user.getUsername(), user.getPassword()));
 
             db.session().save(secuser);
         } catch (Exception e) {
@@ -133,9 +141,9 @@ public class PMSecurityDBConnector implements PMSecurityConnector {
         }
     }
 
-    public void updateUser(PMSecurityUser user)    throws PMSecurityException{
+    public void updateUser(PMSecurityUser user) throws PMSecurityException {
         DB db = (DB) ctx.get(PM_DB);
-        try{
+        try {
             checkUserRules(user.getUsername(), user.getPassword());
             SECUser secuser = getDBUser(user.getUsername());
             secuser = unconvert(secuser, user);
@@ -144,58 +152,55 @@ public class PMSecurityDBConnector implements PMSecurityConnector {
             getLog().error(e);
         }
     }
-    
-    public PMSecurityUserGroup getGroup(String groupname)                throws PMSecurityException{
+
+    public PMSecurityUserGroup getGroup(String groupname) throws PMSecurityException {
         return convert(getDBGroup(groupname));
     }
-    
-    public SECUserGroup getDBGroup(String groupname){
+
+    public SECUserGroup getDBGroup(String groupname) {
         DB db = (DB) ctx.get(PM_DB);
         SECUserGroup g = null;
         try {
-            g = (SECUserGroup) db.session()
-                                    .createCriteria (SECUserGroup.class)
-                                    .add (Restrictions.eq ("name", groupname))
-                                    .uniqueResult();
+            g = (SECUserGroup) db.session().createCriteria(SECUserGroup.class).add(Restrictions.eq("name", groupname)).uniqueResult();
         } catch (Exception e) {
             getLog().error(e);
         }
         return g;
     }
-    
-    public List<PMSecurityUserGroup> getGroups()                        throws PMSecurityException{
+
+    public List<PMSecurityUserGroup> getGroups() throws PMSecurityException {
         DB db = (DB) ctx.get(PM_DB);
         List<PMSecurityUserGroup> groups = new ArrayList<PMSecurityUserGroup>();
         try {
             List<SECUserGroup> ug = db.session().createCriteria(SECUserGroup.class).list();
-            for(SECUserGroup g : ug){
+            for (SECUserGroup g : ug) {
                 groups.add(convert(g));
             }
         } catch (Exception e) {
             getLog().error(e);
-        } finally{
-            
+        } finally {
         }
         return groups;
     }
 
-    public void addGroup(PMSecurityUserGroup group)                        throws PMSecurityException{
+    public void addGroup(PMSecurityUserGroup group) throws PMSecurityException {
         DB db = (DB) ctx.get(PM_DB);
-        try{
-            if(getDBGroup(group.getName()) != null) 
+        try {
+            if (getDBGroup(group.getName()) != null) {
                 throw new GroupAlreadyExistException();
-            
-            SECUserGroup secuserg = unconvert(null,group);
+            }
+
+            SECUserGroup secuserg = unconvert(null, group);
 
             db.session().save(secuserg);
         } catch (Exception e) {
             getLog().error(e);
         }
     }
-    
-    public void updateGroup(PMSecurityUserGroup group)                    throws PMSecurityException{
+
+    public void updateGroup(PMSecurityUserGroup group) throws PMSecurityException {
         DB db = (DB) ctx.get(PM_DB);
-        try{
+        try {
             SECUserGroup secuserg = getDBGroup(group.getName());
             db.session().refresh(secuserg);
             secuserg = unconvert(secuserg, group);
@@ -204,28 +209,29 @@ public class PMSecurityDBConnector implements PMSecurityConnector {
             getLog().error(e);
         }
     }
-    
-    public List<PMSecurityPermission> getPermissions()                throws PMSecurityException{
+
+    public List<PMSecurityPermission> getPermissions() throws PMSecurityException {
         List<PMSecurityPermission> perms = new ArrayList<PMSecurityPermission>();
         DB db = (DB) ctx.get(PM_DB);
-        try{
+        try {
             List<SECPermission> ps = db.session().createCriteria(SECPermission.class).list();
-            for(SECPermission p : ps){
+            for (SECPermission p : ps) {
                 perms.add(convert(p));
             }
         } catch (Exception e) {
-            getLog().error(e);            
-        } finally{
-            
-        }        
+            getLog().error(e);
+        } finally {
+        }
         return perms;
     }
-    
+
     /*Converters*/
-    
-    private PMSecurityUser convert(SECUser u) throws PMSecurityException {
-        if(u==null) return null;
+    protected PMSecurityUser convert(SECUser u) throws PMSecurityException {
         PMSecurityUser user = new PMSecurityUser();
+        load(u, user);
+        return user;
+    }
+    protected void load(SECUser u, PMSecurityUser user) throws PMSecurityException {
         user.setActive(u.isActive());
         user.setChangePassword(u.isChangePassword());
         user.setDeleted(u.isDeleted());
@@ -233,80 +239,100 @@ public class PMSecurityDBConnector implements PMSecurityConnector {
         user.setName(u.getName());
         user.setPassword(u.getPassword());
         user.setUsername(u.getNick());
-        for(SECUserGroup g : u.getGroups()){
+        for (SECUserGroup g : u.getGroups()) {
             user.getGroups().add(convert(g));
         }
-        return user;
     }
-        
-    private PMSecurityUserGroup convert(SECUserGroup g) {
-        if(g==null)return null;
+
+    protected PMSecurityUserGroup convert(SECUserGroup g) {
+        if (g == null) {
+            return null;
+        }
         PMSecurityUserGroup group = new PMSecurityUserGroup();
         group.setActive(g.isActive());
         group.setDescription(g.getDescription());
         group.setName(g.getName());
-        for(SECPermission p : g.getPermissions()){
+        for (SECPermission p : g.getPermissions()) {
             group.getPermissions().add(convert(p));
         }
-            
+
         return group;
     }
 
-    private PMSecurityPermission convert(SECPermission p) {
-        if(p==null) return null;
+    protected PMSecurityPermission convert(SECPermission p) {
+        if (p == null) {
+            return null;
+        }
         PMSecurityPermission perm = new PMSecurityPermission();
         perm.setDescription(p.getDescription());
         perm.setName(p.getName());
         return perm;
     }
 
-    private SECUser unconvert(SECUser secuser, PMSecurityUser u) {
-        if(u==null) return null;
-        SECUser user = secuser;
-        if(secuser == null)    user = new SECUser();
-        user.getGroups().clear();
-        user.setActive(u.isActive());
-        user.setChangePassword(u.isChangePassword());
-        user.setDeleted(u.isDeleted());
-        user.setEmail(u.getEmail());
-        user.setName(u.getName());
-        if(secuser==null) user.setNick(u.getUsername().toLowerCase());
-        for(PMSecurityUserGroup g : u.getGroups()){
-            user.getGroups().add(getDBGroup(g.getName()));
+    protected SECUser unconvert(SECUser secuser, PMSecurityUser u) {
+        if (u == null) {
+            return null;
         }
+        SECUser user = secuser;
+        if (secuser == null) {
+            user = new SECUser();
+        }
+        unload(u, secuser, user);
         return user;
     }
 
-    private SECUserGroup unconvert(SECUserGroup secgroup, PMSecurityUserGroup g) {
-        if(g==null)return null;
+    protected void unload(PMSecurityUser u, SECUser secuser, SECUser output) {
+        output.getGroups().clear();
+        output.setActive(u.isActive());
+        output.setChangePassword(u.isChangePassword());
+        output.setDeleted(u.isDeleted());
+        output.setEmail(u.getEmail());
+        output.setName(u.getName());
+        if (secuser == null) {
+            output.setNick(u.getUsername().toLowerCase());
+        }
+        for (PMSecurityUserGroup g : u.getGroups()) {
+            output.getGroups().add(getDBGroup(g.getName()));
+        }
+    }
+
+    protected SECUserGroup unconvert(SECUserGroup secgroup, PMSecurityUserGroup g) {
+        if (g == null) {
+            return null;
+        }
         SECUserGroup group = secgroup;
-        if(secgroup==null) group = new SECUserGroup();
+        if (secgroup == null) {
+            group = new SECUserGroup();
+        }
         group.setActive(g.isActive());
         group.setDescription(g.getDescription());
-        if(secgroup==null)group.setName(g.getName());
-        for(PMSecurityPermission p : g.getPermissions()){
+        if (secgroup == null) {
+            group.setName(g.getName());
+        }
+        for (PMSecurityPermission p : g.getPermissions()) {
             group.getPermissions().add(getDBPerm(p.getName()));
         }
         return group;
     }
 
-    private SECPermission getDBPerm(String name) {
+    protected SECPermission getDBPerm(String name) {
         DB db = (DB) ctx.get(PM_DB);
         SECPermission p = null;
         try {
-            p = (SECPermission) db.session()
-                                    .createCriteria (SECPermission.class)
-                                    .add (Restrictions.eq ("name", name))
-                                    .uniqueResult();
-            if(p!=null)db.session().refresh(p);
+            p = (SECPermission) db.session().createCriteria(SECPermission.class).add(Restrictions.eq("name", name)).uniqueResult();
+            if (p != null) {
+                db.session().refresh(p);
+            }
         } catch (Exception e) {
             getLog().error(e);
-        } 
+        }
         return p;
     }
 
-    private SECPermission unconvert(PMSecurityPermission p) {
-        if(p==null) return null;
+    protected SECPermission unconvert(PMSecurityPermission p) {
+        if (p == null) {
+            return null;
+        }
         SECPermission perm = new SECPermission();
         perm.setDescription(p.getDescription());
         perm.setName(p.getName());
@@ -314,20 +340,18 @@ public class PMSecurityDBConnector implements PMSecurityConnector {
     }
 
     public void setContext(PMContext ctx) {
-        this.ctx = ctx;        
+        this.ctx = ctx;
     }
 
-    public void addProfile(PMSecurityProfile profile)throws PMSecurityException {
+    public void addProfile(PMSecurityProfile profile) throws PMSecurityException {
         // TODO Auto-generated method stub
     }
 
-    
     public PMSecurityProfile getProfile(String id) throws PMSecurityException {
         // TODO Auto-generated method stub
         return null;
     }
 
-    
     public List<PMSecurityProfile> getProfiles() throws PMSecurityException {
         // TODO Auto-generated method stub
         return null;
@@ -336,24 +360,21 @@ public class PMSecurityDBConnector implements PMSecurityConnector {
     public void removeGroup(PMSecurityUserGroup group)
             throws PMSecurityException {
         DB db = (DB) ctx.get(PM_DB);
-        db.session().delete( getDBGroup(group.getName()) );
+        db.session().delete(getDBGroup(group.getName()));
     }
-    
+
     public void removeProfile(PMSecurityProfile profile)
             throws PMSecurityException {
         // TODO Auto-generated method stub
-        
     }
 
-    
     public void updateProfile(PMSecurityProfile profile)
             throws PMSecurityException {
         // TODO Auto-generated method stub
-        
     }
 
     public void removeUser(PMSecurityUser object) throws PMSecurityException {
         DB db = (DB) ctx.get(PM_DB);
-        db.session().delete( getDBUser(object.getUsername()) );
+        db.session().delete(getDBUser(object.getUsername()));
     }
 }
