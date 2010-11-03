d76177ae753323e8dae2e279ec71425ccdb177cd
jpaoletti <jeronimo.paoletti@gmail.com>
2010-11-03 10:32:26 -0300

    Formatting

--- modules/pm_struts/src/org/jpos/ee/pm/struts/actions/MonitorAction.java
+++ modules/pm_struts/src/org/jpos/ee/pm/struts/actions/MonitorAction.java
@@ -16,43 +16,48 @@ import org.jpos.ee.pm.struts.StrutsMonitorObserver;
 public class MonitorAction extends ActionSupport {
 
     private static final String PM_MONITOR_WATCHER = "pm.monitor.watcher";
-    protected boolean checkUser(){     return false;}
 
+    @Override
+    protected boolean checkUser() {
+        return false;
+    }
+
+    @Override
     protected boolean prepare(PMStrutsContext ctx) throws PMException {
         synchronized (ctx.getSession()) {
             super.prepare(ctx);
             String c = ctx.getParameter("continue");
-            boolean kontinue = (c!=null) && (c.compareTo("true")==0);
+            boolean kontinue = (c != null) && (c.compareTo("true") == 0);
             ctx.put(PM_MONITOR_CONTINUE, kontinue);
-            
-            String pmid = ctx.getRequest().getParameter(PM_ID);
-            if(pmid==null) {
-                pmid=(String) ctx.getSession().getAttribute(LAST_PM_ID);
-            }else{
-                ctx.getSession().setAttribute(LAST_PM_ID,pmid);
+
+            String pmid = ctx.getRequest().getParameter(PMEntitySupport.PM_ID);
+            if (pmid == null) {
+                pmid = (String) ctx.getSession().getAttribute(PMEntitySupport.LAST_PM_ID);
+            } else {
+                ctx.getSession().setAttribute(PMEntitySupport.LAST_PM_ID, pmid);
             }
             Monitor monitor = ctx.getPresentationManager().getMonitor(pmid);
-            if(monitor == null) {
+            if (monitor == null) {
                 ctx.getErrors().add(new PMMessage(ActionMessages.GLOBAL_MESSAGE, "pm.struts.error.monitor.not.found", pmid));
                 throw new PMException();
             }
-            ctx.put(PM_MONITOR, monitor);        
+            ctx.put(PM_MONITOR, monitor);
             return true;
         }
     }
-    
+
     protected void doExecute(PMStrutsContext ctx) throws PMException {
         synchronized (ctx.getSession()) {
             boolean kontinue = (Boolean) ctx.get(PM_MONITOR_CONTINUE);
             Monitor monitor = (Monitor) ctx.get(PM_MONITOR);
-            MonitorObserver watcher = (MonitorObserver)ctx.getSession().getAttribute(PM_MONITOR_WATCHER);
+            MonitorObserver watcher = (MonitorObserver) ctx.getSession().getAttribute(PM_MONITOR_WATCHER);
             List<String> lines = new ArrayList<String>();
-            if(!kontinue || watcher==null){
+            if (!kontinue || watcher == null) {
                 watcher = new StrutsMonitorObserver(monitor);
                 ctx.getSession().setAttribute(PM_MONITOR_WATCHER, watcher);
-            }else{
+            } else {
                 try {
-                    lines.addAll( watcher.getLines() );
+                    lines.addAll(watcher.getLines());
                 } catch (Exception e) {
                     ctx.getPresentationManager().error(e);
                     lines.add("ERROR C");
@@ -60,8 +65,9 @@ public class MonitorAction extends ActionSupport {
             }
             ctx.getSession().setAttribute("pm_monitor_lines", lines);
             ctx.getSession().setAttribute("monitor", monitor);
-            if(kontinue)
+            if (kontinue) {
                 throw new PMForwardException("monitor_cont");
+            }
         }
     }
 }
